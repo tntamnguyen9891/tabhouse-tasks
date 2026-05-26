58226554c2df7d6fbc290ed73a8658bfe9e15196
tntamnguyen9891 <tntamnguyen9891@gmail.com>
2026-05-26 11:04:18 +0700

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 export const supabase = createClient(
-  "https://cfasnpdmdahdbgjcutlq.supabase.co",
-  "sb_publishable_KlkYZrIUbifTfNVgDtMD5Q_8fKwOKs_"
+  "https://gwihocbcugdfpzwjoepk.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3aWhvY2JjdWdkZnB6d2pvZXBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3NTc0NDEsImV4cCI6MjA5NTMzMzQ0MX0.qS_ReCNEGngLfoPM4K8XmXKKgGSbN-X7hTKpBPzGHQs"
 )
